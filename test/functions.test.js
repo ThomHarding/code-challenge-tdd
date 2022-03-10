35287d931dca951113db6ce67e17db0a6a6cbac5
THOMAS HARDING <hardingt604@gmail.com>
2022-03-10 14:08:26 -0700

    finished 3 tests + plaintext explanations for all functions

--- test/functions.test.js
+++ test/functions.test.js
@@ -111,7 +111,39 @@ test('returnAsAnArrayBasic', (expect) => {
     expect.deepEqual(actual, expected, 'the three inputs made into an array of length 3');
 });
 
+test('returnAsAnArray2', (expect) => {
+    const expected = [8, 4, 5];
+
+    const actual = returnAsAnArray(8, 4, 5);
+
+    expect.deepEqual(actual, expected, 'the three inputs made into an array of length 3');
+});
+
+test('returnAsAnArray3', (expect) => {
+    const expected = [8, 4, 5];
+
+    const actual = returnAsAnArray(8, 4, 5);
+
+    expect.deepEqual(actual, expected, 'the three inputs made into an array of length 3');
+});
+
 test('returnAsAStringBasic', (expect) => {
+    const expected = 2813;
+
+    const actual = returnAsAString(2, 8, 13);
+
+    expect.equal(actual, expected, 'returns all three inputs concatenated together into a string');
+});
+
+test('returnAsAString2', (expect) => {
+    const expected = 2813;
+
+    const actual = returnAsAString(2, 8, 13);
+
+    expect.equal(actual, expected, 'returns all three inputs concatenated together into a string');
+});
+
+test('returnAsAString3', (expect) => {
     const expected = 2813;
 
     const actual = returnAsAString(2, 8, 13);
@@ -127,10 +159,42 @@ test('makeLuckyGreetingBasic', (expect) => {
     expect.equal(actual, expected, 'returns inputs added together after flavour text string');
 });
 
+test('makeLuckyGreetingNegative', (expect) => {
+    const expected = 'Hello! Your lucky number for the day is -753.';
+
+    const actual = makeLuckyGreeting(-800, 47);
+
+    expect.equal(actual, expected, 'returns inputs added together after flavour text string, but with negatives');
+});
+
+test('makeLuckyGreetingStrings', (expect) => {
+    const expected = 'Hello! Your lucky number for the day is oh no not a string!.';
+
+    const actual = makeLuckyGreeting('oh no', ' not a string!');
+
+    expect.equal(actual, expected, 'adds strings together and adds to end of flavour text string');
+});
+
 test('getSecondItemBasic', (expect) => {
     const expected = 'apple';
 
     const actual = getSecondItem(['kiwi', 'apple', 'orange', 'plum']);
+
+    expect.equal(actual, expected, 'returns the second item in the array');
+});
+
+test('getSecondItemOneItemArray', (expect) => {
+    const expected = undefined;
+
+    const actual = getSecondItem(['apple']);
+
+    expect.equal(actual, expected, 'undefined, as it attempts to access a blank index');
+});
+
+test('getSecondItemMoreBasicer', (expect) => {
+    const expected = 'a most excellent hat';
+
+    const actual = getSecondItem(['a suave pair of shoes', 'a most excellent hat', 'a most groovy coat of paint']);
 
     expect.equal(actual, expected, 'returns the second item in the array');
 });
@@ -143,6 +207,31 @@ test('getLastItemBasic', (expect) => {
     expect.equal(actual, expected, 'returns the last item of the array');
 });
 
+test('getLastItemOneItemArray', (expect) => {
+    const expected = 'plum';
+
+    const actual = getLastItem(['plum']);
+
+    expect.equal(actual, expected, 'returns the last and only item of the array');
+});
+
+test('getLastItemEmptyArray', (expect) => {
+    const expected = undefined;
+
+    const actual = getLastItem([]);
+
+    expect.equal(actual, expected, 'undefined as the array has no items to get the last one of');
+});
+
 test('getRandomNumberBasic', (expect) => {  
     expect.equal((1 <= getRandomNumber() <= 5), true, 'returns a random number between 1 and 5 inclusive');
+});
+
+test('getRandomNumberTypeOf', (expect) => {
+    const returnType = typeof getRandomNumber();
+    expect.equal(returnType, 'number', 'makes sure the function actually returns a Number object');
+});
+
+test('getRandomNumberAssertInteger', (expect) => {  
+    expect.equal((getRandomNumber() % 1 === 0), true, 'checks that the number is an integer (aka number mod 1 is equal to 0)');
 });
